47bc71976ca865ce65529451f1514269fb841cc7
abhishek22331 <abhishek.immanent@gmail.com>
2024-03-07 10:02:15 +0530

    build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import {
 const inter = Inter({ subsets: ["latin"] });
 import ABI from "../../abi/abi.js";
 import { useAccount, useReadContract } from "wagmi";
+import Link from "next/link.js";
 
 export default function Home() {
   const { open } = useWeb3Modal();
@@ -79,7 +80,7 @@ export default function Home() {
       <main>
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a
+            <Link
               href="/"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
@@ -87,7 +88,7 @@ export default function Home() {
               <span className="address self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Polarfi
               </span>
-            </a>
+            </Link>
             <button
               data-collapse-toggle="navbar-default"
               type="button"
@@ -118,21 +119,21 @@ export default function Home() {
             >
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <a
+                  <Link
                     href="#"
                     className="address block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     href="#"
                     className="address block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     About
-                  </a>
+                  </Link>
                 </li>
                 {/* <li>
                   <a
